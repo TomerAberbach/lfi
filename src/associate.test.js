b3e4fb22055b6d6ad6edb14115a9585d65a0041b
Tomer Aberbach <tomeraberbach@gmail.com>
2020-12-18 11:04:56 -0500

    feat: simplify associate

--- src/associate.test.js
+++ src/associate.test.js
@@ -7,24 +7,6 @@ testProp(
   (t, array, fn) => {
     const map = new Map()
 
-    t.is(associateTo(fn, { map, put: (m, k, v) => m.set(k, v) }, array), map)
-  }
-)
-
-testProp(
-  `associateTo calls the given put function for every result in the proper order`,
-  [fc.array(fc.anything()), fc.func(fc.tuple(fc.string(), fc.anything()))],
-  (t, array, fn) => {
-    const expected = array.map(value => fn(value))
-    const actual = associateTo(
-      fn,
-      {
-        map: [],
-        put: (map, k, v) => map.push([k, v])
-      },
-      array
-    )
-
-    t.deepEqual(actual, expected)
+    t.is(associateTo(fn, map, array), map)
   }
 )

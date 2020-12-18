38d3f182a33adb51d08d8eab26d490c78a9cb39d
Tomer Aberbach <tomeraberbach@gmail.com>
2020-12-18 11:58:09 -0500

    feat: predicate

---
+++ src/predicate.js
@@ -0,0 +1,27 @@
+import { curry } from './curry.js'
+
+export const all = curry((fn, iterable) => {
+  for (const value of iterable) {
+    if (fn(value) !== true) {
+      return false
+    }
+  }
+
+  return true
+})
+
+export const any = curry((fn, iterable) => {
+  for (const value of iterable) {
+    if (fn(value) === true) {
+      return true
+    }
+  }
+
+  return false
+})
+
+export const none = curry((fn, iterable) => !any(fn, iterable))
+
+export const contains = curry((searchValue, iterable) =>
+  any(value => Object.is(value, searchValue), iterable)
+)

91648ec73b64253a7f900c605fcb8829885f4bfa
Tomer Aberbach <tomeraberbach@gmail.com>
2020-12-18 00:50:57 -0500

    feat: windoweD

---
+++ src/windowed.js
@@ -0,0 +1,28 @@
+import { curry } from './curry.js'
+import { toExtendedIterator } from './to-extended-iterator.js'
+
+export const windowed = curry(function* (size, iterable) {
+  const iterator = toExtendedIterator(iterable[Symbol.iterator]())
+  const window = []
+
+  for (let i = 0; i < size - 1; i++) {
+    if (!iterator.hasNext()) {
+      return
+    }
+
+    window.push(iterator.getNext())
+  }
+
+  let start = 0
+  while (iterator.hasNext()) {
+    window[(start + size - 1) % size] = iterator.getNext()
+
+    const currentWindow = []
+    for (let i = 0; i < size; i++) {
+      currentWindow.push(window[(start + i) % size])
+    }
+
+    yield currentWindow
+    start = (start + 1) % size
+  }
+})

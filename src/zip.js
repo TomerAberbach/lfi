2f021a88e35453b53afc75fd9b7b724400da1ae2
Tomer Aberbach <tomeraberbach@gmail.com>
2020-12-18 12:01:29 -0500

    feat: zip

---
+++ src/zip.js
@@ -0,0 +1,11 @@
+import { toExtendedIterator } from './to-extended-iterator.js'
+
+export function* zip(...iterables) {
+  const iterators = iterables.map(iterable =>
+    toExtendedIterator(iterable[Symbol.iterator]())
+  )
+
+  while (iterators.every(iterator => iterator.hasNext())) {
+    yield iterators.map(iterator => iterator.getNext())
+  }
+}
